72bc824f8c3877845f95026e9a67a073d27b23b2
pavi <pavithracr2001@gmail.com>
2023-08-16 13:56:47 +0530

    new

--- Constructors and Destructor/ParameterisedC.cpp
+++ Constructors and Destructor/ParameterisedC.cpp
@@ -21,3 +21,45 @@ int main()
     cout << demo.i << "&" << demo.f <<endl;
     cout << demo2.i << "&" << demo2.f <<endl;
 }
+
+
+
+
+/*By using Parameterised 
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+class AddString
+{
+    
+    char string1[20],string2[20];
+    public:
+    
+    void operator+()
+    {
+        cout << "After concatination : " << strcat(string1,string2);
+        
+    }
+    AddString(char* str1,char* str2)
+    {
+        strcpy(string1,str1);
+        strcpy(string2,str2);
+    }
+};
+int main() 
+{
+    
+    char str1[]= "Prep";
+    char str2[]="Insta";
+    
+    AddString object(str1,str2);
+    
+    
+    +object;
+    
+    return 0;
+}
+
+
+*/
